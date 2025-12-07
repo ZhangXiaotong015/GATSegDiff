2651e399611e9efe5ad5d40eaf9abc4f2e19a3c6
Xiaotong Zhang <31834504+ZhangXiaotong015@users.noreply.github.com>
2025-12-07 16:59:54 +0100

    Update run.sh

--- Dockerfile/GATSegDiff/run.sh
+++ Dockerfile/GATSegDiff/run.sh
@@ -9,7 +9,7 @@ docker run --rm --gpus "device=0" \
     -v /mnt/e/WSL/TestData/LiverVesselSeg/Pre-ablation/Portal:/data_test_CT \
     -v "$data_test_CT_25D":/data_test_CT_25D \
     -e MODEL_PATH=/app/model/LiVS_reannotated_subset_F0_model160000.pt \
-    gat_seg_diff:20251125 \
+    gat_seg_diff:latest \
     --out_dir /output \
     --data_test_CT /data_test_CT \
     --test_root_path /data_test_CT_25D \
@@ -30,7 +30,7 @@ docker run --rm --gpus "device=0" \
 #     -v /mnt/e/WSL/TestData/LiverVesselSeg/Pre-ablation/Portal:/data_test_CT \
 #     -v "$data_test_CT_25D":/data_test_CT_25D \
 #     -e MODEL_PATH=/app/model/LiVS_reannotated_subset_F0_model160000.pt \
-#     gat_seg_diff:20251125 \
+#     gat_seg_diff:latest \
 #     bash
 
 ##### All mounted paths must be exist!!!
